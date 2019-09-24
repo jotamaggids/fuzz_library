ecbbe4c1c7d8407eb3d920d552313c3f4c78c0d2
jotamaggi <jotamaggi@gmail.com>
2019-09-24 09:04:50 -0300

    Add Cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Fuzzy Library example using Pandas',
+    author='Jeremias Maggi',
+    license='MIT',
+)
